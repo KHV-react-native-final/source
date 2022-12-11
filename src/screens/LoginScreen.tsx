5794073a13593daec0b6d6d8c208a2deeb67e765
HieuDEVELOPER <hieunguyentx0010@gmail.com>
2022-12-11 11:54:13 +0700

    Update login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,8 +1,22 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {ImageBackground, StyleSheet, View} from 'react-native';
+import {
+  Dimensions,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import BaseBtn from '../components/core/BaseBtn';
 import BaseInput from '../components/core/BaseInput';
 export default () => {
+  const navigation = useNavigation();
+  const onPress = () => {
+    navigation.navigate('RegisterScreen' as never);
+  };
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -10,27 +24,28 @@ export default () => {
           uri: 'https://wecsaigon.com/wp-content/uploads/2021/11/good-girl.jpg',
         }}
         style={styles.img}>
-        <View style={styles.loginForm}>
-          <BaseInput label="UserName:" placeholder="Enter your username" />
-          <BaseInput label="Password" placeholder="Enter your password" />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{alignItems: 'center', justifyContent: 'center'}}>
+          <View style={styles.loginForm}>
+            <BaseInput label="UserName:" placeholder="Enter your username" />
+            <BaseInput label="Password" placeholder="Enter your password" />
 
-          {/* <View>
-            <Text style={styles.boxTittle}>User Name</Text>
-            <TextInput style={styles.input} placeholder="Enter your email..." />
+            <View style={styles.Btn}>
+              <BaseBtn title="Login" />
+            </View>
+            <TouchableOpacity onPress={onPress}>
+              <Text
+                style={{
+                  color: '#000',
+                  textAlign: 'center',
+                  marginTop: 20,
+                }}>
+                Register Now
+              </Text>
+            </TouchableOpacity>
           </View>
-
-          <View>
-            <Text style={styles.boxTittle}>Pass Word</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your password..."
-            />
-          </View> */}
-
-          <View style={styles.Btn}>
-            <BaseBtn tittle="Login" />
-          </View>
-        </View>
+        </KeyboardAvoidingView>
       </ImageBackground>
     </View>
   );
@@ -38,22 +53,17 @@ export default () => {
 const styles = StyleSheet.create({
   container: {},
   img: {
-    position: 'absolute',
-    height: 845,
+    height: Dimensions.get('window').height + 25,
     width: '100%',
-    elevation: 2,
-    top: '0%',
     opacity: 0.7,
   },
   loginForm: {
-    marginTop: 370,
-    elevation: 10,
     backgroundColor: '#fff',
     borderRadius: 10,
     paddingBottom: 25,
     paddingTop: 20,
-    marginLeft: 5,
-    marginRight: 5,
+    marginTop: '60%',
+    width: '100%',
   },
   Btn: {
     justifyContent: 'center',
